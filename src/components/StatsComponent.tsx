bf5f7df3bedeed8186b8ff9a8443b03740b51bfb
Vinaigre <olivier.savarino+sufod@gmail.com>
2022-11-16 19:42:21 +0100

    finalized refacto (some bugs are still present)

--- src/components/StatsComponent.tsx
+++ src/components/StatsComponent.tsx
@@ -7,7 +7,13 @@ type Props = {
 class StatsComponent extends React.Component<Props>
 {
   render() {
-    return <div className="relative-height stats">(Work In Progress)</div>;
+    return <div className="relative-height stats">
+      <p>(Work In Progress)</p>
+      <p>Cet outil est en plein développement et risque de contenir beaucoup de bugs, mais il sera mis à jour très régulièrement. (Je ne suis pas responsable si un ordinateur prend feu :eyes:)</p>
+      <p>Je suis ouvert à toutes recommandations à propos de l'outil mais j'ai déjà beaucoup d'idées qui attendent d'être implémentées.</p>
+      <p>Fait par Vinaigre (Discord: Vinaigre#4083)</p>
+      <p><a style={{ color: "yellow" }} href="https://github.com/Vinaigre1/dofus-trap-network">Le code est open-source !</a></p>
+    </div>;
   }
 }
 
